6f7fdccc5631aeab82cc45aba23bd1081f2091d7
Александр Мехаев <alexandmekhaev@yandex.ru>
2024-11-18 19:40:19 +0300

    cringe

--- exceptions.py
+++ exceptions.py
@@ -6,9 +6,25 @@ class TokenError(Exception):
         return 'Токены не были найдены'
 
 
-class IncorrectTypeOfResponse(Exception):
+class ResponseTypeError(Exception):
     """Ответ API не удалось привести к типу данных Python."""
 
     def __str__(self):
         """Ответ API не удалось привести к типу данных Python."""
-        return 'Ответ API не удалось привести к типу данных Python'
+        return 'Ответ API не удалось привести к типу данных Python.'
+
+
+class HomeworkIndexError(Exception):
+    """От API не было получено ни одной записи."""
+
+    def __str__(self):
+        """От API не было получено ни одной записи."""
+        return 'От API не было получено ни одной записи.'
+
+
+class ServerResponseError(Exception):
+    """Не удалось получить успешный ответ от API."""
+
+    def __str__(self):
+        """Не удалось получить успешный ответ от API."""
+        return 'Не удалось получить успешный ответ от API.'
--- homework.py
+++ homework.py
@@ -7,17 +7,17 @@ from pprint import pprint
 from dotenv import load_dotenv
 from telebot import TeleBot, types
 
-from exceptions import TokenError, IncorrectTypeOfResponse
+from exceptions import TokenError, ResponseTypeError, HomeworkIndexError, ServerResponseError
 
 load_dotenv()
 
-practicum_token = os.getenv('PRACTICUM_TOKEN')
-telegram_token = os.getenv('TELEGRAM_TOKEN')
-telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {practicum_token}'}
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
 HOMEWORK_VERDICTS = {
@@ -29,56 +29,67 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     """Ищет токены в переменных окружения."""
-    if practicum_token and telegram_token and telegram_chat_id:
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
         return
     else:
         raise TokenError
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет результат пользователю в телеграмм."""
 
 
 def get_api_answer(timestamp):
     """Получает ответ от API и приводит его к типу данных Python."""
-    payload = {'from_date': 0}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+    try:
+        payload = {'from_date': timestamp}
+        response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+    except requests.RequestException as error:
+        message = f'Сбой в работе программы: {error}'
+        print(message)
+    if response.status_code != 200:
+        raise ServerResponseError
     return response.json()
 
 
 def check_response(response):
-    """Проверяет тип данных ответа API."""
+    """..."""
+    if 'homeworks' not in response:
+        raise TypeError
     if not isinstance(response, dict):
-        raise IncorrectTypeOfResponse
+        raise TypeError
+    if not isinstance(response['homeworks'], list):
+        raise TypeError
 
 
 def parse_status(homework):
-    homework_name = homework['homework_name']
-    verdict = homework['status']
+    """Подготавливает ответ API."""
+    homework_name = homework['homeworks'][0]['homework_name']
+    verdict = HOMEWORK_VERDICTS[homework['homeworks'][0]['status']]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
 
 
 def main():
     """Основная логика работы бота."""
-
     # Создаем объект класса бота
-    bot = TeleBot(token=telegram_token)
+    check_tokens()
+    bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
 
-    check_tokens()
-
     while True:
-        response = get_api_answer(timestamp)
-        check_response(response)
-        homework = response['homeworks'][0]
-        print(parse_status(homework))
         try:
-            ...
-
+            test = get_api_answer(timestamp)
+            check_response(test)
+            parse_status(test)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            print(message)
 
-        bot.polling()
+        time.sleep(600)
+        # bot.polling()
 
 if __name__ == '__main__':
     main()
+
+
